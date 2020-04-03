db1957818757f3713693344da93b7da2f7b0eba3
Jade <knowingplus@hotmail.com>
2020-04-03 22:23:04 +0800

    后端项目创建及依赖配置

---
+++ kmall-server/kmall-manage/kmall-manage-web/src/main/java/plus/knowing/kmall/KMallApplication.java
@@ -0,0 +1,12 @@
+package plus.knowing.kmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KMallApplication.class);
+    }
+}
