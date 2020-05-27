398fac327131746e0521042afc6aa192ac8beaeb
Jade <knowingplus@hotmail.com>
2020-05-27 23:09:54 +0800

    file模块

---
+++ kmall-file/src/main/java/plus/knowing/kmall/KMallFileApplication.java
@@ -0,0 +1,12 @@
+package plus.knowing.kmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KMallFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KMallFileApplication.class, args);
+    }
+}
