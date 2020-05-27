9cdcf33a7e2041b2bf7376f08892e8630e142110
Jade <knowingplus@hotmail.com>
2020-05-27 22:33:18 +0800

    项目配置

--- kmall-server/kmall-manage/kmall-manage-web/src/main/java/plus/knowing/kmall/KMallApplication.java
+++ kmall-server/kmall-manage/kmall-manage-web/src/main/java/plus/knowing/kmall/KMallApplication.java
@@ -1,8 +1,10 @@
 package plus.knowing.kmall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(basePackages = {"plus.knowing.kmall.dao"})
 @SpringBootApplication
 public class KMallApplication {
 
